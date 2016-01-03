07047b3212f458db0b1dc4c6664f92af315b4195
Feiyi Zhan <zhanfeiyi_sh@msn.com>
2016-01-02 22:22:47 -0800

    MAINT: make word search ii faster

--- src/solutions/WordSearchII.java
+++ src/solutions/WordSearchII.java
@@ -8,7 +8,6 @@ import java.util.*;
 public class WordSearchII {
     class TrieNode {
         TrieNode[] children = new TrieNode[26];
-        char childrenCnt;
         boolean isWord;
     }
 
@@ -16,35 +15,36 @@ public class WordSearchII {
         if (board == null || board.length == 0) return new ArrayList<>();
 
         TrieNode trie = _buildTrie(words);
-        Set<String> found = new HashSet<>();
-        char[][] color = new char[board.length][board[0].length];
+        List<String> found = new ArrayList<>();
+        char[][] visited = new char[board.length][board[0].length];
         int[][] moves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[i].length; ++j)
-                _find(i, j, moves, color, board, trie, "", found);
+                _find(i, j, moves, visited, board, trie, "", found);
         }
-        return new ArrayList<>(found);
+        return found;
     }
 
-    private void _find(int startRow, int startCol, int[][] moves, char[][] color, char[][] board,
-                       TrieNode node, String word, Set<String> found) {
-        if (node.childrenCnt == 0) return;
-        color[startRow][startCol] = 1;
-        for (int i = 0; i < 26; ++i)
-            if (node.children[i] != null && board[startRow][startCol] == 'a' + i) {
-                word += board[startRow][startCol];
-                node = node.children[i];
-                if (node.isWord) found.add(word);
-                for (int[] move : moves) {
-                    int nextRow = startRow + move[0], nextCol = startCol + move[1];
-                    if (nextRow >= 0 && nextRow < board.length && nextCol >= 0 && nextCol < board[0].length
-                            && color[nextRow][nextCol] == 0) {
-                        _find(nextRow, nextCol, moves, color, board, node, word, found);
-                    }
-                }
-                break;
+    private void _find(int startRow, int startCol, int[][] moves, char[][] visited, char[][] board,
+                       TrieNode node, String word, List<String> found) {
+        visited[startRow][startCol] = 1;
+        int i = board[startRow][startCol] - 'a';
+        if (node.children[i] != null) {
+            word += board[startRow][startCol];
+            node = node.children[i];
+            if (node.isWord) {
+                node.isWord = false;
+                found.add(word);
             }
-        color[startRow][startCol] = 0;
+            for (int[] move : moves) {
+                int nextRow = startRow + move[0], nextCol = startCol + move[1];
+                if (nextRow >= 0 && nextRow < board.length && nextCol >= 0 && nextCol < board[0].length
+                        && visited[nextRow][nextCol] == 0) {
+                    _find(nextRow, nextCol, moves, visited, board, node, word, found);
+                }
+            }
+        }
+        visited[startRow][startCol] = 0;
     }
 
     private TrieNode _buildTrie(String[] words) {
@@ -53,10 +53,7 @@ public class WordSearchII {
             TrieNode ptr = root;
             for (int i = 0; i < word.length(); ++i) {
                 int ahead = word.charAt(i) - 'a';
-                if (ptr.children[ahead] == null) {
-                    ptr.children[ahead] = new TrieNode();
-                    ++ptr.childrenCnt;
-                }
+                if (ptr.children[ahead] == null) ptr.children[ahead] = new TrieNode();
                 ptr = ptr.children[ahead];
                 if (i == word.length() - 1)
                     ptr.isWord = true;
@@ -81,7 +78,7 @@ public class WordSearchII {
                     "ihkr".toCharArray(),
                     "iflv".toCharArray()
             };
-            String[] word = {"oath","pea","eat","rain"};
+            String[] word = {"oath", "pea", "eat", "rain"};
             test(board, word);
         }
     }
