c979041eed64e115fcc93147a41507ce82abef2a
feiyi.zhan <feiyi.zhan@servicenow.com>
2016-01-28 15:29:21 -0800

    MAINT: solution to "Find the Peak Element"

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-import solutions.ContiuousSubarraySumII;
+import solutions.FindPeakElement;
 
 /**
  * Created by PPlovboiledfish on 10/31/15.
@@ -6,7 +6,7 @@ import solutions.ContiuousSubarraySumII;
 public class Test {
     static public void main(String[] args) {
         long startTime = System.nanoTime();
-        ContiuousSubarraySumII.Test.randomTest();
+        FindPeakElement.Test.randomTest();
         System.out.println( "Total Time: " + (System.nanoTime() - startTime) / 1000000 + "ms");
     }
 }
