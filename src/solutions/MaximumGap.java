8d9695aa635ecf365c02684b9d20d3a792cd9720
Feiyi Zhan <zhanfeiyi_sh@msn.com>
2016-01-28 21:58:27 -0800

    MAINT: solution to "Maximum Gap"

--- src/solutions/MaximumGap.java
+++ src/solutions/MaximumGap.java
@@ -7,29 +7,33 @@ public class MaximumGap {
     public int maximumGap(int[] nums) {
         if (nums == null || nums.length < 2)
             return 0;
-        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         for (int num : nums) {
-            max = Math.max(max, num);
             min = Math.min(min, num);
+            max = Math.max(max, num);
         }
-        int period = (max - min) / nums.length + 1;
-        int[][] holes = new int[(max - min) / period + 1][3];
+
+        int interval = (max - min) / nums.length + 1;
+        int count = (max - min) / interval + 1;
+        int[][] holes = new int[count][];
         for (int num : nums) {
-            int hole = (num - min) / period;
-            if (holes[hole][2] == 0) {
-                holes[hole][0] = num;
-                holes[hole][1] = num;
+            int hole = (num - min) / interval;
+            if (holes[hole] == null) {
+                holes[hole] = new int[]{num, num};
             } else {
-                holes[hole][0] = Math.max(num, holes[hole][0]);
-                holes[hole][1] = Math.min(num, holes[hole][1]);
+                holes[hole][0] = Math.min(holes[hole][0], num);
+                holes[hole][1] = Math.max(holes[hole][1], num);
             }
-            ++holes[hole][2];
         }
-        int maxGap = 0, lastElem = min;
-        for (int[] hole : holes) {
-            if (hole[2] != 0) {
-                maxGap = Math.max(maxGap, hole[1] - lastElem);
-                lastElem = hole[0];
+
+        int maxGap = 0, last = -1;
+        for (int i = 0; i < count; ++i) {
+            if (holes[i] != null) {
+                if (last >= 0) {
+                    maxGap = Math.max(maxGap, holes[i][0] - last);
+                }
+                last = holes[i][1];
             }
         }
         return maxGap;
