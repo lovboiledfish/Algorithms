c6903c15e4d5e9a44d370f4ea30d4cea4833db7e
feiyi.zhan <feiyi.zhan@servicenow.com>
2016-02-01 17:52:03 -0800

    MAINT: make n-queens faster

--- src/solutions/NQueens.java
+++ src/solutions/NQueens.java
@@ -8,17 +8,23 @@ import java.util.List;
  * Created by feiyi.zhan on 2/1/16.
  */
 public class NQueens {
+	// sets to optimize validation process
+	private char[] _cols, _sums, _diffs;
+	// result
+	private List<List<String>> _patterns;
+
 	public List<List<String>> solveNQueens(int n) {
-		List<List<String>> patterns = new ArrayList<>();
+		_patterns = new ArrayList<>();
 		if (n > 0) {
-			int[][] board = new int[n][n];
-			_dfs(new ArrayList<>(), 0, board, patterns);
+			_cols = new char[n];
+			_sums = new char[n << 1];
+			_diffs = new char[n << 1];
+			_dfs(n, new ArrayList<>(), 0);
 		}
-		return patterns;
+		return _patterns;
 	}
 
-	private void _dfs(List<int[]> queens, int row, int[][] board, List<List<String>> res) {
-		int n = board.length;
+	private void _dfs(int n, List<int[]> queens, int row) {
 		if (row == n) {
 			char[] rw = new char[n];
 			Arrays.fill(rw, '.');
@@ -28,56 +34,27 @@ public class NQueens {
 				ans.add(new String(rw));
 				rw[queen[1]] = '.';
 			}
-			res.add(ans);
+			_patterns.add(ans);
 		} else {
 			for (int i = 0; i < n; ++i) {
-				if (board[row][i] == 0) {
+				int sum = row + i;
+				int diff = row - i + n;
+				if (_cols[i] == 0 && _sums[sum] == 0 && _diffs[diff] == 0) {
 					int[] queen = {row, i};
-					_placeQueen(board, queen, 1);
 					queens.add(queen);
-					_dfs(queens, row + 1, board, res);
+					_setQueen(i, sum, diff, (char)1);
+					_dfs(n, queens, row + 1);
 					queens.remove(queens.size() - 1);
-					_placeQueen(board, queen, -1);
+					_setQueen(i, sum, diff, (char)0);
 				}
 			}
 		}
 	}
 
-	private void _placeQueen(int[][] board, int[] queen, int fill) {
-		int n = board.length;
-
-		// left diagonal start point
-		int rL = 0, cL = 0;
-		int deltaR = queen[0] - queen[1];
-		if (deltaR > 0) {
-			rL = deltaR;
-		} else {
-			cL = -deltaR;
-		}
-		// right diagonal start point
-		int rR = n - 1, cR = 0;
-		int deltaL = (n - 1 - queen[0]) - queen[1];
-		if (deltaL > 0) {
-			rR = n - 1 - deltaL;
-		} else {
-			cR = -deltaL;
-		}
-
-		for (int i = 0; i < n; ++i) {
-			// row
-			board[queen[0]][i] += fill;
-			// column
-			board[i][queen[1]] += fill;
-			// left diagonal
-			if (rL + i < n && cL + i < n) {
-				board[rL + i][cL + i] += fill;
-			}
-			// right diagonal
-			if (rR - i >= 0 && cR + i < n) {
-				board[rR - i][cR + i] += fill;
-			}
-		}
-		board[queen[0]][queen[1]] -= 3 * fill;
+	private void  _setQueen(int col, int sum, int diff, char val) {
+		_cols[col] = val;
+		_sums[sum] = val;
+		_diffs[diff] = val;
 	}
 
 	static public class Test {
