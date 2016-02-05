523708ab7283f0d5ac8ac4914fc4b84e5c1c2cd6
Feiyi Zhan <zhanfeiyi_sh@msn.com>
2016-02-04 20:49:33 -0800

    MAINT: faster solution to "Find the Duplicate Number"

--- src/solutions/FindTheDuplicateNumber.java
+++ src/solutions/FindTheDuplicateNumber.java
@@ -5,21 +5,17 @@ package solutions;
  */
 public class FindTheDuplicateNumber {
     public int findDuplicate(int[] nums) {
-        int low = 0, high = nums.length - 1;
-        while (low < high) {
-            int mid = (low + high) >> 1;
-            int count = 0;
-            for (int num : nums) {
-                if (num <= mid) {
-                    ++count;
-                }
-            }
-            if (count > mid) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return low;
+        return fast;
     }
 }
