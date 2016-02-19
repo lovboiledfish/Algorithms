f04054f36f29693ee426b10cf011bcd25b5926be
feiyi.zhan <feiyi.zhan@servicenow.com>
2016-02-18 21:58:21 -0800

    MAINT: trim the solution to "construct binary tree from preorder and in order traversal"

--- src/solutions/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/solutions/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,9 +1,9 @@
 package solutions;
 
-import solutions.utils.TreeNode;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import solutions.utils.TreeNode;
 
 /**
  * Created by PPlovboiledfish on 2/8/16.
@@ -14,15 +14,18 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
             return null;
 
         final int n = preorder.length;
-        Map<Integer, Integer> inorderIdx = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> inorderIdx = new HashMap<>();
         for (int i = 0; i < n; ++i) {
             inorderIdx.put(inorder[i], i);
         }
-        return _buildTree(preorder, 0, inorder, 0, n, inorderIdx);
+        return _buildTree(preorder, 0, 0, n, inorderIdx);
     }
 
-    private TreeNode _buildTree(int[] preorder, int pStart, int[] inorder,
-                                int iStart, int size, Map<Integer, Integer> inorderIdx) {
+    private TreeNode _buildTree(int[] preorder,
+                                int pStart,
+                                int iStart,
+                                int size,
+                                Map<Integer, Integer> inorderIdx) {
         if (size <= 0) {
             return null;
         }
@@ -30,8 +33,8 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         int val = preorder[pStart];
         TreeNode node = new TreeNode(val);
         int leftSize = inorderIdx.get(val) - iStart;
-        node.left = _buildTree(preorder, pStart + 1, inorder, iStart, leftSize, inorderIdx);
-        node.right = _buildTree(preorder, pStart + 1 + leftSize, inorder, iStart + leftSize + 1, size - leftSize - 1, inorderIdx);
+        node.left = _buildTree(preorder, pStart + 1, iStart, leftSize, inorderIdx);
+        node.right = _buildTree(preorder, pStart + 1 + leftSize, iStart + leftSize + 1, size - leftSize - 1, inorderIdx);
         return node;
     }
 }
