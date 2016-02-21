b60ea75e94a7d86cb8c353c5b82afef8bbeb0556
feiyi.zhan <feiyi.zhan@servicenow.com>
2016-02-20 20:59:53 -0800

    MAINT: solution to "Four Sum"

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-import solutions.AmicablePair;
+import solutions.FourSum;
 
 /**
  * Created by PPlovboiledfish on 10/31/15.
@@ -6,7 +6,7 @@ import solutions.AmicablePair;
 public class Test {
     static public void main(String[] args) {
         long startTime = System.nanoTime();
-        AmicablePair.Test.randomTest();
+        FourSum.Test.randomTest();
         System.out.println( "Total Time: " + (System.nanoTime() - startTime) / 1000000 + "ms");
     }
 }
